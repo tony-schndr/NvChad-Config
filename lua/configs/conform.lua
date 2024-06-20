b1efe772147a869712e498b2ba839ac7fbea9c69
Tony Schneider <tonyschndr@gmail.com>
2024-06-19 22:28:01 -0500

    add goimports to formatter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,7 +1,7 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    go = {"gofmt"}
+    go = {"gofmt", "goimports"}
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -37,6 +37,7 @@ return {
         "css-lsp",
         "prettier",
         "gopls",
+        "goimports",
       },
     },
   },
