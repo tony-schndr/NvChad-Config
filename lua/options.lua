16c253ce10be0cf84491090876f306e9f83fcf87
Tony Schneider <tonyschndr@gmail.com>
2024-09-06 20:45:45 -0500

    display hidden files

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,7 @@ require "nvchad.options"
 local conf = require "nvchad.configs.nvimtree"
 conf.git = conf.git or {} -- Ensure the git table exists
 conf.git.ignore = false
+conf.filters.dotfiles = false
 
 -- add yours here!
 -- o.cursorlineopt ='both' -- to enable cursorline!
